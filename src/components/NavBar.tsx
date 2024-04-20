f2eb35d8462a9d5870f52b64b3e5908dfe6947ff
chihebgit <chiheb.samti@gmail.com>
2024-04-21 00:19:10 +0200

    build  Hero sliec

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,6 +3,7 @@ import WordMark from "@/components/WordMark";
 import { Content } from "@prismicio/client";
 import { PrismicNextLink } from "@prismicio/next";
 import Link from "next/link";
+import ButtonLink from "@/components/ButtonLink";
 
 type NavBarProps = {
   settings: Content.SettingsDocument;
@@ -18,11 +19,12 @@ const NavBar = ({ settings }: NavBarProps) => {
         </Link>
         <ul className="flex gap-6">
           {settings.data.navigation.map((item) => {
-
-            if(item.cta_button){
-              return(
-                <ButtonLink />
-              )
+            if (item.cta_button) {
+              return (
+                <li key={item.label}>
+                  <ButtonLink field={item.link}>{item.label}</ButtonLink>
+                </li>
+              );
             }
 
             return (
