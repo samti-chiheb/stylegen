490d61cd66be21f91161167c0d4a34154f4ab470
chihebgit <chiheb.samti@gmail.com>
2024-04-30 09:51:53 +0200

    update navbar & add CallToAction slice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,6 +30,5 @@ export default async function Index() {
   // The client queries content from the Prismic API
   const client = createClient();
   const home = await client.getByUID("page", "home");
-
   return <SliceZone slices={home.data.slices} components={components} />;
 }
